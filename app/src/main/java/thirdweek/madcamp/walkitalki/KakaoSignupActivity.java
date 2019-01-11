e567a7da2f9b3d4dc04607a2b19461792d1146a5
송동욱 <quino0627@korea.ac.kr>
2019-01-11 15:17:11 +0900

    kakao login works

---
+++ app/src/main/java/thirdweek/madcamp/walkitalki/KakaoSignupActivity.java
@@ -0,0 +1,73 @@
+package thirdweek.madcamp.walkitalki;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.kakao.auth.ErrorCode;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.ApiErrorCode;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.MeResponseCallback;
+import com.kakao.usermgmt.callback.MeV2ResponseCallback;
+import com.kakao.usermgmt.response.MeV2Response;
+import com.kakao.usermgmt.response.model.UserProfile;
+import com.kakao.util.OptionalBoolean;
+import com.kakao.util.helper.log.Logger;
+
+public class KakaoSignupActivity extends Activity {
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestMe();
+    }
+
+    /**
+     * 사용자의 상태를 알아 보기 위해 me API 호출을 한다.
+     */
+    protected void requestMe() { //유저의 정보를 받아오는 함수
+
+        UserManagement.getInstance().me(new MeV2ResponseCallback() {
+            @Override
+            public void onFailure(ErrorResult errorResult) {
+                String message = "failed to get user info. msg=" + errorResult;
+                Logger.d(message);
+
+                int result = errorResult.getErrorCode();
+                if (result == ApiErrorCode.CLIENT_ERROR_CODE) {
+                    finish();
+                } else {
+                    redirectLoginActivity();
+                }
+            }
+
+            @Override
+            public void onSessionClosed(ErrorResult errorResult) {
+                Logger.e("onSessionClosed");
+                redirectLoginActivity();
+            }
+
+            @Override
+            public void onSuccess(MeV2Response result) {
+
+                redirectMainActivity();
+
+            }
+        });
+    }
+
+    private void redirectMainActivity() {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+    protected void redirectLoginActivity() {
+        final Intent intent = new Intent(this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        startActivity(intent);
+        finish();
+    }
+
+
+}
--- app/src/main/java/thirdweek/madcamp/walkitalki/MainActivity.java
+++ app/src/main/java/thirdweek/madcamp/walkitalki/MainActivity.java
@@ -1,20 +1,44 @@
 package thirdweek.madcamp.walkitalki;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.MenuItem;
 
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.ApiErrorCode;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.MeV2ResponseCallback;
+import com.kakao.usermgmt.response.MeV2Response;
+import com.kakao.util.helper.log.Logger;
+
+import java.security.MessageDigest;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mContext = getApplicationContext();
+        getHashKey(mContext);
+
+        this.requestMe();
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
@@ -51,4 +75,72 @@ public class MainActivity extends AppCompatActivity {
                 }
             };
 
+
+    @Nullable
+    public static String getHashKey(Context context) {
+        final String TAG = "KeyHash";
+        String keyHash = null;
+        try {
+            PackageInfo info =
+                    context.getPackageManager().getPackageInfo(context.getPackageName(), PackageManager.GET_SIGNATURES);
+
+            for (Signature signature : info.signatures) {
+                MessageDigest md;
+                md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                keyHash = new String(Base64.encode(md.digest(), 0));
+                Log.d(TAG, keyHash);
+            }
+        } catch (Exception e) {
+            Log.e("name not found", e.toString());
+        }
+
+        if (keyHash != null) {
+            return keyHash;
+        } else {
+            return null;
+        }
+    }
+
+    protected void requestMe() { //유저의 정보를 받아오는 함수
+
+        UserManagement.getInstance().me(new MeV2ResponseCallback() {
+            @Override
+            public void onFailure(ErrorResult errorResult) {
+                String message = "failed to get user info. msg=" + errorResult;
+                Logger.d(message);
+
+                int result = errorResult.getErrorCode();
+                if (result == ApiErrorCode.CLIENT_ERROR_CODE) {
+                    finish();
+                } else {
+                    redirectLoginActivity();
+                }
+            }
+
+            @Override
+            public void onSessionClosed(ErrorResult errorResult) {
+                Logger.e("onSessionClosed");
+                redirectLoginActivity();
+            }
+
+            @Override
+            public void onSuccess(MeV2Response result) {
+
+                redirectMainActivity();
+
+            }
+        });
+    }
+
+    private void redirectMainActivity() {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+    protected void redirectLoginActivity() {
+        final Intent intent = new Intent(this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        startActivity(intent);
+        finish();
+    }
 }
